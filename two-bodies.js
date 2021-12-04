b0967d17e13520b71fcc45d72387148b24a79df4
ccarlosuarezz <karlosuarez710@gmail.com>
2021-12-04 12:32:36 -0500

    Add parameters and info labels

--- two-bodies.js
+++ two-bodies.js
@@ -1,10 +1,28 @@
+const inputEarthGravity = document.getElementById('earth_gravity');
+const inputSunGravity = document.getElementById('sun_gravity');
+const inputEarthMass = document.getElementById('earth_mass');
+const inputSunMass = document.getElementById('sun_mass');
+const inputEarthDiameter = document.getElementById('earth_diameter');
+const inputSunDiameter = document.getElementById('sun_diameter');
+const inputYears = document.getElementById('years');
+const inputSimulationTime = document.getElementById('simulation_time');
 
+const pGravityEarth = document.getElementById('pGravityEarth');
+const pGravitySun = document.getElementById('pGravitySun');
+const pMassEarth = document.getElementById('pMassEarth');
+const pMassSun = document.getElementById('pMassSun');
+const pDiameterEarth = document.getElementById('pDiameterEarth');
+const pDiameterSun = document.getElementById('pDiameterSun');
+const pYears = document.getElementById('pYears');
+const pSimulationTime = document.getElementById('pSimulationTime');
 
 const startButton = document.getElementById('start_button');
 const pauseButton = document.getElementById('pause_button');
 const playButton = document.getElementById('play_button');
 const canvas = document.getElementById('solar_system');
 let context = canvas.getContext('2d');
+
+const SECOND_IN_MILLI = 1000;
 
 playButton.addEventListener('click', play);
 pauseButton.addEventListener('click', pause);
@@ -18,12 +36,16 @@ let orbitWidth = 300;
 let orbitHeight = 250;
 // let sunDesphase = orbitWidth*0.0084;
 let sunDesphase = orbitWidth*0.3;
+let years = 1;
+let simulationTime = 10;
 
 let sun = {
     url: 'cool_sun.svg',
     isLoad: false,
-    mass: 0,
-    size: 100
+    gravity: 274,
+    mass: 1.99,
+    diameter: 1391016,
+    size: 1391016/10000
 }
 sun.image = new Image();
 sun.image.src = sun.url;
@@ -32,8 +54,10 @@ sun.image.addEventListener("load", loadSun);
 let earth = {
     url: 'kawaii_earth.svg',
     isLoad: false,
-    mass: 0,
-    size: 40
+    gravity: 9.81,
+    mass: 5.97,
+    diameter: 12742,
+    size: 12742/1000
 }
 earth.image = new Image();
 earth.image.src = earth.url;
@@ -43,6 +67,7 @@ function loadSun() {
     sun.isLoad = true;
     if (earth.isLoad && sun.isLoad) {
         drawSolarSystem(0);
+        resetInfo();
     }
 }
 
@@ -50,13 +75,13 @@ function loadEarth() {
     earth.isLoad = true;
     if (earth.isLoad && sun.isLoad) {
         drawSolarSystem(0);
+        resetInfo();
     }
 }
 
 function draw() {
     if (earth.isLoad && sun.isLoad) {
         let i = 0;
-        let years = 10;
         let isDelay = false;
         let diference = 0;
         
@@ -130,6 +155,37 @@ function reset() {
         isSimulation = false;
         isPause = false;
         clearInterval(drawInerval);
+        //parameters
+        if (inputEarthGravity.value) {earth.gravity = inputEarthGravity.value}
+        if (inputEarthMass.value) {earth.mass = inputEarthMass.value}
+        if (inputEarthDiameter.value) {
+            earth.diameter = inputEarthDiameter.value;
+            earth.size = inputEarthDiameter.value/1000;
+        }
+
+        if (inputSunGravity.value) {sun.gravity = inputSunGravity.value}
+        if (inputSunMass.value) {sun.mass = inputSunMass.value}
+        if (inputSunDiameter.value) {
+            sun.diameter = inputSunDiameter.value;
+            sun.size = inputSunDiameter.value/10000;
+        }
+
+        if (inputYears.value) {years = inputYears.value}
+        if (inputSimulationTime.value) {simulationTime = inputSimulationTime.value}
+
         drawSolarSystem(0);
+
+        resetInfo();
     }
+}
+
+function resetInfo() {
+    pGravityEarth.innerHTML = `Gravedad Tierra: ${earth.gravity} m/s<sup>2</sup>`;
+    pGravitySun.innerHTML = `Gravedad Sol: ${sun.gravity} m/s<sup>2</sup>`;
+    pMassEarth.innerHTML = `Masa Tierra: ${earth.mass} x10<sup>24</sup> kg`;
+    pMassSun.innerHTML = `Masa Sol: ${sun.mass} x10<sup>30</sup> kg`;
+    pDiameterEarth.innerHTML = `Diametro Tierra: ${earth.diameter} km`;
+    pDiameterSun.innerHTML = `Diametro Sol: ${sun.diameter} km`;
+    pYears.innerHTML = `Años a simular: ${years}`;
+    pSimulationTime.innerHTML = `Tiempo simulación: ${simulationTime} s`;
 }
